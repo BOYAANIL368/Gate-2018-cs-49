252ecae1d05b83b056fe9e87eb53354dd27254b1
BOYAANIL368 <boyaanilkumar2003@gmail.com>
2025-04-01 09:45:25 +0530

    C program

--- prime.c
+++ prime.c
@@ -1,43 +1,81 @@
 #include <stdio.h>
+#define NUM_VARS 4 
+void generateBooleanExpression(int kmap[4][4]) {
+    char vars[NUM_VARS] = {'A', 'B', 'C', 'D'};
 
-int main() {
-    int minterms[] = {0, 2, 5, 7, 9, 11};
-    int dont_cares[] = {3, 8, 10, 12, 14};
-    int kmap[4][4] = {0};
-    for (int i = 0; i < 6; i++) {
-        int val = minterms[i];
-        int row = (val >> 2) & 3; 
-        int col = val & 3;       
-        kmap[row][col] = 1;
-    }
-    for (int i = 0; i < 5; i++) {
-        int val = dont_cares[i];
-        int row = (val >> 2) & 3;
-        int col = val & 3;      
-        kmap[row][col] = -1; 
-    }
-    printf("K-map:\n");
-    printf("PQ\\RS | 00 | 01 | 11 | 10 |\n");
-    printf("------|----|----|----|----|\n");
+    printf("Simplified Boolean Expression: ");
     for (int i = 0; i < 4; i++) {
-        printf(" %02d |", i);
         for (int j = 0; j < 4; j++) {
-            if (kmap[i][j] == 1) {
-                printf(" 1 |");
-            } else if (kmap[i][j] == -1) {
-                printf(" d |");
-            } else {
-                printf(" 0 |");
+            if (kmap[i][j] == 1 || kmap[i][j] == -1) { 
+                if (i == 0) {
+                    if (j == 0) {
+                        printf("A'B'C'D'");
+                    } else if (j == 1) {
+                        printf("A'B'C'D");
+                    } else if (j == 2) {
+                        printf("A'BC'D'");
+                    } else {
+                        printf("A'BC'D");
+                    }
+                } else if (i == 1) {
+                    if (j == 0) {
+                        printf("AB'C'D'");
+                    } else if (j == 1) {
+                        printf("AB'C'D");
+                    } else if (j == 2) {
+                        printf("ABC'D'");
+                    } else {
+                        printf("ABC'D");
+                    }
+                } else if (i == 2) {
+                    if (j == 0) {
+                        printf("A'B'C'D'");
+                    } else if (j == 1) {
+                        printf("A'B'C'D");
+                    } else if (j == 2) {
+                        printf("A'BC'D'");
+                    } else {
+                        printf("A'BC'D");
+                    }
+                } else if (i == 3) {
+                    if (j == 0) {
+                        printf("AB'C'D'");
+                    } else if (j == 1) {
+                        printf("AB'C'D");
+                    } else if (j == 2) {
+                        printf("ABC'D'");
+                    } else {
+                        printf("ABC'D");
+                    }
+                }
+                if (i != 3 || j != 3) {
+                    printf(" + ");
+                }
             }
         }
-        printf("\n");
     }
-    printf("\nEssential Prime Implicants:\n");
-    printf("P'Q'R'\n");
-    printf("P'QR\n");
-    printf("PQ'R'\n");
+    printf("\n");
+}
 
-    printf("\nNumber of Essential Prime Implicants: 3\n");
-
-    return 0;
+void main() {
+    int kmap[4][4];
+    printf("Enter the K-map values (0, 1, or X for don't care) for a 4-variable K-map:\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            char input;
+            printf("Enter value for cell [%d][%d] (either 0, 1, or X for don't care): ", i, j);
+            scanf(" %c", &input); 
+            if (input == '1') {
+                kmap[i][j] = 1;
+            } else if (input == '0') {
+                kmap[i][j] = 0;
+            } else if (input == 'X' || input == 'x') {
+                kmap[i][j] = -1;  
+            } else {
+                printf("Invalid input. Please enter 0, 1, or X.\n");
+                j--; 
+            }
+        }
+    }
+    generateBooleanExpression(kmap);
 }
